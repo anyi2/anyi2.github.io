753aa8d70e3204a41de576fc3587cfe7b2e10a21
安逸 <40451747+anyi98@users.noreply.github.com>
2018-08-12 21:30:00 +0800

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -4,6 +4,6 @@ var service = $.url('?service');
 var query = $.url('?query');
 var nextUrl = $.url('?next');
 if (nextUrl) {
-    $('#next-url').text('正在跳转到页面').append($('<a>').addClass('sub header').attr('href', nextUrl).text(service + '：' + query));
+    $('#next-url').text('即将搭载本站IP，探索世界未知！').append($('<a>').addClass('sub header').attr('href', nextUrl).text(service + '：' + query));
     window.location.replace(nextUrl);
 };
