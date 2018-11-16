85b254ca8e3d1d32fe560e005d7f00304daa282c
安逸 <40451747+anyi98@users.noreply.github.com>
2018-11-17 00:29:44 +0800

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -4,6 +4,6 @@ var service = $.url('?service');
 var query = $.url('?query');
 var nextUrl = $.url('?next');
 if (nextUrl) {
-    $('#next-url').text('探索未知的征程从未止步').append($('<a>').addClass('sub header').attr('href', nextUrl).text(service + '：' + query));
+    $('#next-url').text('安逸导航丨自由的探索互联网').append($('<a>').addClass('sub header').attr('href', nextUrl).text(service + '：' + query));
     window.location.replace(nextUrl);
 };
