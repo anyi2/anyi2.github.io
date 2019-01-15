00a6b3bb8f8959b5137b941fc2e89abeb0db7eea
安逸 <40451747+anyi98@users.noreply.github.com>
2019-01-15 20:06:08 +0800

    Update hitokoto.js

--- assets/js/hitokoto.js
+++ assets/js/hitokoto.js
@@ -1,1 +1,10 @@
-ANYI.LIFE
+// hitokoto.js	ANYI.LIFE
+
+ $.getJSON('https://v1.hitokoto.cn', function (data) {	
+    $('#hitokoto').attr({	
+        'href': '//v1.hitokoto.cn?id=' + data.id,	
+        'target': '_blank'	
+    }).append(data.hitokoto).append(	
+        $('<div>').addClass('detail').text(data.from)	
+    );	
+});
