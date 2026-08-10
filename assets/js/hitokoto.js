8bb77da50b6ef78b6262500e5219c18b72a3ccf0
anyi <40451747+anyi2@users.noreply.github.com>
2026-08-11 04:40:06 +0800

    Update hitokoto.js

--- assets/js/hitokoto.js
+++ assets/js/hitokoto.js
@@ -1,10 +1,15 @@
-// hitokoto.js	ANYI.LIFE
+// hitokoto.js
 
- $.getJSON('https://v1.hitokoto.cn', function (data) {	
-    $('#hitokoto').attr({	
-        'href': '//v1.hitokoto.cn?id=' + data.id,	
-        'target': '_blank'	
-    }).append(data.hitokoto).append(	
-        $('<div>').addClass('detail').text(data.from)	
-    );	
+$.getJSON('https://v1.hitokoto.cn', function (data) {
+    $('#hitokoto').attr({
+        'href': `//hitokoto.cn?id=${data.id}`,
+        'target': '_blank'
+    }).append(data.hitokoto).append(
+        $('<div>').addClass('detail').text(data.from)
+    );
+}).fail(function () {
+    $('#hitokoto').attr({
+        'href': 'https://www.anyi2.com/',
+        'target': '_blank'
+    }).html('<i class="lightbulb icon"></i>全新升级的新版学术导航<div class="detail">安逸导航</div>');
 });
